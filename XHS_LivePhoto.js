2bbba5f390b2c0ff50a4002e83b0a2d19dae9cb7
AmigoYao <112379343+AmigoYao@users.noreply.github.com>
2024-10-16 18:40:58 +0800

    Update XHS_LivePhoto.js

--- XHS_LivePhoto.js
+++ XHS_LivePhoto.js
@@ -10,4 +10,4 @@
 hostname = sns-video-bd.xhscdn.com
 
 # response-body
-https://edith.xiaohongshu.com/api/sns/v1/note/live_photo/save url response-body http://sns-video-qc.xhscdn.com/stream/\d+/\d+/[a-zA-Z0-9]+_\d+.mp4(?:\?sign=[a-zA-Z0-9]+\&t=.{8})? response-body AmigoYao
+https://edith.xiaohongshu.com/api/sns/v1/note/live_photo/save url response-body http://sns-video-.*.xhscdn.com/stream/\d+/\d+/[a-zA-Z0-9]+_\d+.mp4(?:\?sign=[a-zA-Z0-9]+&t=.{8})? response-body AmigoYao
