8862da6068fb768930b1c8f1364a33b16cc63bca
AmigoYao <112379343+AmigoYao@users.noreply.github.com>
2026-02-06 19:48:30 +0800

    Update XueXiTongJinghua.js

--- XueXiTongJinghua.js
+++ XueXiTongJinghua.js
@@ -9,7 +9,7 @@
 # 重写以下域名
 hostname = apps.chaoxing.com, home-yd.chaoxing.com, appswh.chaoxing.com
 
-https://apps.chaoxing.com/apis/recent/getRecord.jspx\?puid\= url response-body \{\"cataName\":\".*?\",\"cataid\":\d+,\"cfid\":-?\d+,\"content\":\{\"resTitle\":\"学生大礼包\",\"resUrl\":\".*?\",\"resLogo\":\".*?\",\"resUid\":\".*?\",\"toolbarType\":\d+,\"allowUpdate2\":true|false\},\"id\":\d+,\"key\":\".*?\",\"norder\":\d+,\"rid\":\d+,\"showInHp\":\d+,\"speTop\":\d+,\"topsign\":\d+,\"updateTime2\":\d{13}\} response-body AmigoYao
+https://apps.chaoxing.com/apis/recent/getRecord.jspx\?puid\= url response-body \{\"cataName\":\".*?\",\"cataid\":\d{9},\"cfid\":-?\d{1},\"content\":\{\"resTitle\":\"学生大礼包\",\"resUrl\":\".*?\",\"resLogo\":\".*?\",\"resUid\":\".*?\",\"toolbarType\":\d{1},\"allowUpdate2\":true|false\},\"id\":\d{2},\"key\":\".*?\",\"norder\":\d{1},\"rid\":\d{8},\"showInHp\":\d{1},\"speTop\":\d{1},\"topsign\":\d{1},\"updateTime2\":\d{13}\} response-body AmigoYao
 
 https://home\-yd.chaoxing.com/apis/data/getAdsRecommend\?token\= url reject
 
