1a9b3eed7bf0c9926d7a403c5c08b908839d91be
AmigoYao <112379343+AmigoYao@users.noreply.github.com>
2026-02-06 19:04:31 +0800

    Create XueXiTongJinghua.js

---
+++ XueXiTongJinghua.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @ScriptName        学习通净化
+// @Author            酷安@AmigoYao
+// @MainFunction      学习通首页净化，去除广告和顶部轮播信息流，需要Quantumult X版本1.5.2 844+
+// @UpdateTime        2026-02-06
+// @Attention         如需引用请注明出处，谢谢！
+
+##############################################
+# 重写以下域名
+hostname = apps.chaoxing.com, home-yd.chaoxing.com, appswh.chaoxing.com
+
+https://apps.chaoxing.com/apis/recent/getRecord.jspx\?puid\= url response-body id":12,"key":"common_xueshengquanyi","norder":0,"rid":43060092 response-body AmigoYao
+https://home\-yd.chaoxing.com/apis/data/getAdsRecommend\?token\= url reject
+https://appswh.chaoxing.com/apis/activity/getNoteTopicList\?puid\= url reject
