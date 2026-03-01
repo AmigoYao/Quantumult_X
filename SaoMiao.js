2640e15a50095e2314cb44196d1e02e7bb2fdd4c
AmigoYao <112379343+AmigoYao@users.noreply.github.com>
2026-03-02 03:46:26 +0800

    Update URL and hostname in SaoMiao.js

--- SaoMiao.js
+++ SaoMiao.js
@@ -5,10 +5,10 @@ CamScanner 解锁部分高级特权
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api-cs-cn|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+^https:\/\/api-cs-cn\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
 
 [mitm]
-hostname = ap*.intsig.net
+hostname = api-cs-cn.intsig.net
 **************************/
 let obj = JSON.parse($response.body);
 obj = {"data":{"psnl_vip_property":{"expiry":"2013017600"}}};
