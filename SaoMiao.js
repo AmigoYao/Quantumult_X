f8fd24031a9ec23624b6c7fe034bd6d15b1bdf6d
AmigoYao <112379343+AmigoYao@users.noreply.github.com>
2026-03-02 03:44:54 +0800

    Rename SaoMiao1.js to SaoMiao.js

---
+++ SaoMiao.js
@@ -0,0 +1,15 @@
+/*
+CamScanner 解锁部分高级特权
+
+***************************
+Quantumult X:
+
+[rewrite_local]
+^https:\/\/(api-cs-cn|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+
+[mitm]
+hostname = ap*.intsig.net
+**************************/
+let obj = JSON.parse($response.body);
+obj = {"data":{"psnl_vip_property":{"expiry":"2013017600"}}};
+$done({body: JSON.stringify(obj)});
