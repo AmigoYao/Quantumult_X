12bbffc5abcb195a6e3a3fce5de73b6a52237ea3
AmigoYao <112379343+AmigoYao@users.noreply.github.com>
2026-03-02 02:15:36 +0800

    Update XueXiTongJinghua.js

--- XueXiTongJinghua.js
+++ XueXiTongJinghua.js
@@ -2,15 +2,15 @@
 // @ScriptName        学习通净化
 // @Author            酷安@AmigoYao
 // @MainFunction      学习通首页净化，去除广告和顶部轮播信息流，需要Quantumult X版本1.5.2 844+
-// @UpdateTime        2026-02-06
+// @UpdateTime        2026-03-02
 // @Attention         如需引用请注明出处，谢谢！
 
 ##############################################
 # 重写以下域名
 hostname = apps.chaoxing.com, home-yd.chaoxing.com, appswh.chaoxing.com
 
-https://apps.chaoxing.com/apis/recent/getRecord.jspx\?puid\= url response-body \{\\"cataName\\":\".*?\\",\\"cataid\\":-?\\d+,\\"cfid\\":-?\\d+,\\"content\\":\{\\"resTitle\\":\\"学生大礼包\\",\\"resUrl\\":\".*?\\",\\"resLogo\\":\".*?\\",\\"resUid\\":\\"common_xueshengquanyi\\",\\"toolbarType\\":-?\\d+,\\"allowUpdate2\\":(true|false)\},\\"id\\":-?\\d+,\\"key\\":\\"common_xueshengquanyi\\",\\"norder\\":-?\\d+,\\"rid\\":-?\\d+,\\"showInHp\\":-?\\d+,\\"speTop\\":-?\\d+,\\"topsign\\":-?\\d+,\\"updateTime2\\":\\d+\}\,\{ response-body \{
+https://apps.chaoxing.com/apis/recent/getRecord.jspx\?puid\= url response-body \{"cataName":"网页".*?"resTitle":"学生大礼包".*?"key":"common_xueshengquanyi".*?\},\{ response-body \{
 
-https://home\-yd.chaoxing.com/apis/data/getAdsRecommend\?token\= url reject
+https://home\-yd.chaoxing.com/apis/data/getAdsRecommend\?token\= url response-body ("bannerListV2"\s*:\s*)\[[\s\S]*\] response-body $1[]
 
-https://appswh.chaoxing.com/apis/activity/getNoteTopicList\?puid\= url reject
+https://appswh.chaoxing.com/apis/activity/getNoteTopicList\?puid\= url response-body ("list"\s*:\s*)\[([\s\S]*)\] response-body $1[]
